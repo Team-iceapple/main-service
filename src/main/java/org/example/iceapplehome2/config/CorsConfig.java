9fc18806f335ee339d5e8e30b7439a8f2111db30
Boyeon-Shin <anna007096@gmail.com>
2025-09-03 14:15:51 +0900

    refactor: cors 설정 config 추가

---
+++ src/main/java/org/example/iceapplehome2/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package org.example.iceapplehome2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
+
+
+
