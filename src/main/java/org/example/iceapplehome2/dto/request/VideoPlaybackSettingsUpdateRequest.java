4a7f4f93b355c3732be3b17d3d8ff85795c5fc20
lseonhye <sh133726@naver.com>
2025-09-11 17:32:43 +0900

    resolves #16 chore: 전역 영상 속도 API 제거

--- src/main/java/org/example/iceapplehome2/dto/request/VideoPlaybackSettingsUpdateRequest.java
+++
@@ -1,3 +0,0 @@
-package org.example.iceapplehome2.dto.request;
-
-public record VideoPlaybackSettingsUpdateRequest(Double playbackRate) {}
--- src/main/java/org/example/iceapplehome2/repository/SettingsRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.iceapplehome2.repository;
-import java.util.Optional;
-
-public interface SettingsRepository {
-    Optional<Double> getPlaybackRate();
-    void upsertPlaybackRate(Double playbackRate);
-}
